369dfc8fb9699d658d8096cd66ab2a9fbb882f94
elbasel <abdelrahman.elbasel@gmail.com>
2021-12-03 00:18:02 +0200

    adjusted random color logic

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
-const INITIIAL_WIDTH = 8
+"use strict";
+
+const INITIIAL_WIDTH = 8;
+
 
 
 function fillSketchArea(gridWidth) {
+    
 
     sketchArea.innerHTML = '';
 
     //making sure cells will be a perfect square
-    gridWidth = gridWidth ** 2
+    gridWidth = gridWidth ** 2;
 
     for (let i = 0; i < gridWidth; i++) {
 
         let cell = document.createElement('div');
-        cell.style.flexBasis = (100 / Math.sqrt(gridWidth)) + "%"
-        cell.classList.add('cell')
+        cell.style.flexBasis = (100 / Math.sqrt(gridWidth)).toFixed(2) + "%";
+        cell.classList.add('cell');
         
         //prevent default drag behavior
-        cell.setAttribute('onmousedown', "event.preventDefault ? event.preventDefault() : event.returnValue = false")
+        cell.setAttribute('onmousedown', "event.preventDefault ? event.preventDefault() : event.returnValue = false");
         
         //fillCell will either draw or erase bases on which mouse button is pressed
-        cell.addEventListener('mouseover', (e) => fillCellOnDrag(e))
+        cell.addEventListener('mouseover', (e) => fillCellOnMouseOver(e));
 
         // left click, right click => draw, erase
-        cell.addEventListener('click', (e) => fillCellOnClick(e))
-        cell.addEventListener('contextmenu', (e) => e.target.style.backgroundColor = '')
+        cell.addEventListener('click', (e) => fillCellOnClick(e));
+        cell.addEventListener('contextmenu', (e) => e.target.style.backgroundColor = '');
        
-        sketchArea.appendChild(cell)
+        sketchArea.appendChild(cell);
     }
 
 }
 
-function fillCellOnDrag(e) {
-    let fillColor = randomColorsButton.classList.contains('rainbow-button') ? '#' + Math.floor(Math.random()*16777215).toString(16) : colorInput.value
+
+function getFillColor() {
+    return randomColorsButton.classList.contains('rainbow-button') ? '#' + Math.floor(Math.random()*16777215).toString(16) : colorInput.value;
+}
+
+function fillCellOnMouseOver(e) {
+    let fillColor = getFillColor();
     if(e.buttons === 1) e.target.style.backgroundColor = fillColor;
     if (e.buttons === 2) e.target.style.backgroundColor = '';
 }
 
 function fillCellOnClick(e) {
-    let fillColor = randomColorsButton.classList.contains('rainbow-button') ? '#' + Math.floor(Math.random()*16777215).toString(16) : colorInput.value
+    let fillColor = getFillColor();
     e.target.style.backgroundColor = fillColor;
 }
 
 
 function clearGrid() {
-    let gridCells = document.querySelectorAll('.cell')
-    gridCells.forEach(cell => cell.style.backgroundColor = '')
+    let gridCells = document.querySelectorAll('.cell');
+    gridCells.forEach(cell => cell.style.backgroundColor = '');
 }
 
 
@@ -54,16 +63,17 @@ const sketchArea = document.querySelector('#sketch-area');
 const slider = document.querySelector('input[type="range"')
 const colorPickerDiv = document.querySelector('#color-picker-div');
 const colorInput = document.querySelector('#color-input');
-const clearButton = document.querySelector('#clear-grid-button')
-const randomColorsButton = document.querySelector('#random-colors-button')
+const clearButton = document.querySelector('#clear-grid-button');
+const randomColorsButton = document.querySelector('#random-colors-button');
 
 
-//event listerns
+//event listeners
 colorInput.onchange = (e) => colorPickerDiv.style.backgroundColor = e.target.value;
-slider.onchange = (e) => fillSketchArea(e.target.value)
-clearButton.onclick = (e) => clearGrid()
-randomColorsButton.onclick = (e) => e.target.classList.toggle('rainbow-button')
+slider.onchange = (e) => fillSketchArea(e.target.value);
+clearButton.onclick = (e) => clearGrid();
+randomColorsButton.onclick = (e) => e.target.classList.toggle('rainbow-button');
 
 // Initial Setup
-fillSketchArea(INITIIAL_WIDTH)
-
+fillSketchArea(INITIIAL_WIDTH);
+document.querySelector('output').textContent = `${INITIIAL_WIDTH} × ${INITIIAL_WIDTH}`;
+document.querySelector('input[type="range"').setAttribute('value', INITIIAL_WIDTH);
